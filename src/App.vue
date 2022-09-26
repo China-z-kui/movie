0db5fee93ad3ad2302cff22657248d2bc5a7c9e8
kk <2580155483@qq.com>
2022-09-26 10:49:54 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div>
+    <router-view></router-view>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.scroll-w::-webkit-scrollbar {
+  width: 6px;
+}
+.scroll-w::-webkit-scrollbar-track {
+  background-color: #efefef;
+  width: 6px;
+}
+/* 滚动条滑块 */
+.scroll-w::-webkit-scrollbar-thumb {
+  border-radius: 3px;
+  background-color: #bfbfbf;
+  width: 6px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,96 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/home',
+    name: 'routeName',
+    component: () => import('../views/HomeView.vue'),
+    children:[
+      {
+        path: 'actor-list',
+        name: 'routeName',
+        component: () => import('../views/actor/ActorList.vue'),
+      },
+      {
+        path: 'actor-add',
+        name: 'routeName',
+        component: () => import('../views/actor/ActorAdd.vue'),
+      },
+      {
+        path: 'director-list',
+        name: 'routeName',
+        component: () => import('../views/director/DirectorList.vue'),
+      },
+      {
+        path: 'director-add',
+        name: 'routeName',
+        component: () => import('../views/director/DirectorAdd.vue'),
+      },
+      {
+        path: 'movie-list',
+        name: 'routeName',
+        component: () => import('../views/movie/MovieList.vue'),
+      },
+      {
+        path: 'movie-add',
+        name: 'routeName',
+        component: () => import('../views/movie/MovieAdd.vue'),
+      },
+       {
+        path: 'movie-update/:id',
+        name: 'routeName',
+        component: () => import('../views/movie/MovieUpdate.vue'),
+      },
+      {
+        path: 'thumb-list/:movie_id',
+        name: 'routeName',
+        component: () => import('../views/thumb/ThumbList.vue'),
+      },
+      {
+        path: 'cinema-add',
+        name: 'routeName',
+        component: () => import('../views/cinema/CinemaAdd.vue'),
+      },
+      {
+        path: 'cinema-list',
+        name: 'routeName',
+        component: () => import('../views/cinema/CinemaList.vue'),
+      },
+      {
+        path: 'cinema-update/:id',
+        name: 'routeName',
+        component: () => import('../views/cinema/CinemaUpdate.vue'),
+      },
+      {
+        path: 'cinema-room-list/:id',
+        name: 'routeName',
+        component: () => import('../views/cinema/CinemaRoomList.vue'),
+      },
+      {
+        path: 'cinema-room-seat-template/:id',
+        name: 'routeName',
+        component: () => import('../views/cinema/CinemaRoomSeatTemplate.vue'),
+      }, 
+      {
+        path: '/home/showingon-plan/:id',
+        name: 'routeName',
+        component: () => import('../views/showingon-plan/ShowingonPlanAdd.vue'),
+      },
+      {
+        path: '/home/showingon-plan/list/:id',
+        name: 'routeName',
+        component: () => import('../views/showingon-plan/ShowingonPlanList.vue'),
+      },
+      
+    ],
+   
+  },
+   {
+        path: '/user/login',
+        name: 'routeName',
+        component: () => import('../views/user/Login.vue'),
+      },
 ]
 
 const router = new VueRouter({
